b1c142652e301ead3c8f79c2d9cd700865acc357
Andrey Sivak <andrey.sivak.dev@gmail.com>
2026-01-13 13:13:10 +0100

    chore(format): phpcs fix php code for button

--- includes/components/button.php
+++ includes/components/button.php
@@ -6,7 +6,7 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
@@ -23,37 +23,37 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return string Button HTML or empty string.
  */
 function ft_blocks_render_button( array $args ): string {
-    if ( empty( $args['content'] ) ) {
-        return '';
-    }
+	if ( empty( $args['content'] ) ) {
+		return '';
+	}
 
-    ['button' => $button_class ] = ft_blocks_get_config_classes();
+	['button' => $button_class ] = ft_blocks_get_config_classes();
 
-    $defaults = array(
-        'content'       => '',
-        'base_class'    => '',
-        'variant'       => 'primary',
-        'extra_classes' => array(),
-    );
+	$defaults = array(
+		'content'       => '',
+		'base_class'    => '',
+		'variant'       => 'primary',
+		'extra_classes' => array(),
+	);
 
-    $args = wp_parse_args( $args, $defaults );
+	$args = wp_parse_args( $args, $defaults );
 
-    $classes = array_filter(
-        array_merge(
-            array(
-                $args['base_class'] ? $args['base_class'] . '__button' : '',
-                $button_class,
-                $button_class . '-' . $args['variant'],
-            ),
-            (array) $args['extra_classes']
-        )
-    );
+	$classes = array_filter(
+		array_merge(
+			array(
+				$args['base_class'] ? $args['base_class'] . '__button' : '',
+				$button_class,
+				$button_class . '-' . $args['variant'],
+			),
+			(array) $args['extra_classes']
+		)
+	);
 
-    $attr_string = sprintf( 'class="%s"', esc_attr( implode( ' ', $classes ) ) );
+	$attr_string = sprintf( 'class="%s"', esc_attr( implode( ' ', $classes ) ) );
 
-    return sprintf(
-        '<p %1$s>%2$s</p>',
-        $attr_string,
-        wp_kses_post( $args['content'] )
-    );
+	return sprintf(
+		'<p %1$s>%2$s</p>',
+		$attr_string,
+		wp_kses_post( $args['content'] )
+	);
 }
