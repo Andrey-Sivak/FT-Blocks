ad6995e4134cb27f25a97b2889f50089f8ddab3d
Andrey Sivak <andrey.sivak.dev@gmail.com>
2026-01-18 21:11:08 +0100

    feat(components): make ImagesSlider single reusable component

---
+++ src/components/ImagesSlider/ImagesSlider.jsx
@@ -0,0 +1,112 @@
+import { __ } from '@wordpress/i18n';
+import { ImageUploader } from '../../components';
+import { Button, Icon } from '@wordpress/components';
+import { seen, closeSmall } from '@wordpress/icons';
+import config from '../../../config.json';
+import './style.scss';
+
+const { prefix } = config;
+const baseClass = `${ prefix }-preview-slider`;
+
+const ImagesSlider = ( {
+	setAttributes,
+	images,
+	imagesAttrName,
+	previewImageIndex,
+	setPreviewImageIndex,
+} ) => {
+	const togglePreviewImage = ( index ) => {
+		setPreviewImageIndex( previewImageIndex === index ? null : index );
+	};
+
+	const updateImage = ( index, newImage ) => {
+		const newImages = [ ...images ];
+		newImages[ index ] = newImage;
+		setAttributes( { [ imagesAttrName ]: newImages } );
+
+		setPreviewImageIndex( index );
+	};
+
+	const addImage = () => {
+		const newImages = [
+			...images,
+			{
+				id: null,
+				url: '',
+			},
+		];
+		setAttributes( { [ imagesAttrName ]: newImages } );
+	};
+
+	const removeImage = ( index ) => {
+		if ( previewImageIndex === index ) {
+			setPreviewImageIndex( null );
+		}
+
+		const newImages = [ ...images ];
+		newImages.splice( index, 1 );
+		setAttributes( { [ imagesAttrName ]: newImages } );
+	};
+
+	return (
+		<div className={ `${ baseClass }` }>
+			{ images.map( ( image, index ) => {
+				const isPreviewActive = previewImageIndex === index;
+
+				return (
+					<div
+						key={ index }
+						className={ `${ baseClass }__slide ${
+							image.id ? '' : 'ft-empty'
+						}` }
+					>
+						{ image.id && (
+							<Button
+								className={ `${ baseClass }__slide_show-preview` }
+								icon={
+									<Icon
+										icon={
+											isPreviewActive ? closeSmall : seen
+										}
+									/>
+								}
+								label={
+									isPreviewActive
+										? __( 'Hide preview', 'ft-blocks' )
+										: __( 'Show preview', 'ft-blocks' )
+								}
+								onClick={ () => togglePreviewImage( index ) }
+								size="medium"
+							/>
+						) }
+
+						<ImageUploader
+							image={ image.url }
+							onSelect={ ( media ) =>
+								updateImage( index, {
+									id: media.id,
+									url: media.url,
+								} )
+							}
+							onRemove={ () => removeImage( index ) }
+						/>
+					</div>
+				);
+			} ) }
+
+			<div className={ `${ baseClass }__add-button` }>
+				<Button
+					isPrimary
+					onClick={ addImage }
+					className="ft-button ft-admin-button"
+				>
+					{ images.length
+						? __( 'Add Image', 'ft-blocks' )
+						: __( 'Add First Image', 'ft-blocks' ) }
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default ImagesSlider;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ImageUploader } from './ImageUploader/ImageUploader';
 export { default as FTButton } from './FTButton/FTButton';
 export { default as RemoveButtonCross } from './RemoveButtonCross/RemoveButtonCross';
+export { default as ImagesSlider } from './ImagesSlider/ImagesSlider';
