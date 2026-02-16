d832de98fb6a07989a10959b184cec9e94901ad3
Andrey Sivak <andrey.sivak.dev@gmail.com>
2026-02-16 19:30:00 +0100

    chore(hero): increase autoplay delay

--- src/blocks/hero/view-script.js
+++ src/blocks/hero/view-script.js
@@ -28,7 +28,7 @@ import { Pagination, Autoplay } from 'swiper/modules';
 				slidesPerView: 1,
 				loop: true,
 				autoplay: {
-					delay: 4000,
+					delay: 7000,
 					speed: 1000,
 				},
 				grabCursor: true,
